6f8d7cac5adc1ca1bf1558b0b6af111d09a3a30f
HCG\Patrik.Mihalcin <patrik.mihalcin@embedit.cz>
2018-02-22 19:02:12 +0100

    Use lombok for constructor injection

--- src/main/java/mihalcin/customconverter/TranslationStringToSpecificationStatusEnumConverter.java
+++ src/main/java/mihalcin/customconverter/TranslationStringToSpecificationStatusEnumConverter.java
@@ -1,19 +1,15 @@
 package mihalcin.customconverter;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.rest.webmvc.json.EnumTranslator;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class TranslationStringToSpecificationStatusEnumConverter implements Converter<String, Specification.Status> {
 
     private final EnumTranslator enumTranslator;
-
-    @Autowired
-    public TranslationStringToSpecificationStatusEnumConverter(EnumTranslator enumTranslator) {
-        this.enumTranslator = enumTranslator;
-    }
 
     @Override
     public Specification.Status convert(String source) {
