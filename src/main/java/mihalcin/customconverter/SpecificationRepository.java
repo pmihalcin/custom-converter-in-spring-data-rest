2272b9069da4e2914facaef5d50b8de5ffe0829d
HCG\Patrik.Mihalcin <patrik.mihalcin@embedit.cz>
2018-02-22 19:19:37 +0100

    Add querydsl support

--- src/main/java/mihalcin/customconverter/SpecificationRepository.java
+++ src/main/java/mihalcin/customconverter/SpecificationRepository.java
@@ -1,9 +1,10 @@
 package mihalcin.customconverter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 /**
  * Created by Patrik.Mihalcin on 22.02.2018
  */
-public interface SpecificationRepository extends JpaRepository<Specification, Long> {
+public interface SpecificationRepository extends JpaRepository<Specification, Long>, QueryDslPredicateExecutor<Specification> {
 }
